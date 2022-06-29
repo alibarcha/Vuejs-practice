a3cfc058721dad916995d9562d057b1723bcbff1
Ali Haider <alibarcha910@gmail.com>
2022-06-29 01:24:13 -0700

    Get Data through Api

---
+++ task-15/src/components/Employ.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="employ">
+        <h2>employ data</h2>
+
+          <form action="">
+            <label for="">Name</label>
+            <input type="text" placeholder="Employ Name"><br>
+             <label for="">Email</label>
+            <input type="text" placeholder="Employ Email"><br>
+             <label for="">Password</label>
+            <input type="password" placeholder="Employ Password"><br>
+            <button>Login</button>
+
+        </form>
+
+
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Employ'
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ task-15/src/components/ShowBlogs.vue
@@ -0,0 +1,74 @@
+<template>
+    <div id="blog">
+            <h2>show Blogs from Rest Api</h2><br>
+                <!-- {{showDatas}} -->
+            <table border="1" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Body</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="item in showDatas" v-bind:key="item.id">
+                        <td>{{item.id}}</td>
+                        <td>{{item.title}}</td>
+                        <td>{{item.body}}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <h2>Shows Blogs In Text</h2>
+
+            <div id="blogDiv" v-for="(item,index) in showDatas" v-bind:key="item.id">
+                <h3 >{{index}} : {{item.title}}</h3>
+                <p >{{item.body}}</p>
+            </div>
+            
+    </div>
+</template>
+
+<script>
+export default {
+    name:'ShowBlogs',
+    data(){
+        return {
+            showDatas:[]
+        }
+    },
+    created (){
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
+            this.showDatas=data.body.slice(0,8)
+            console.log(data)
+        });
+
+    }
+}
+</script>
+
+<style scoped>
+ table{
+    width: 70%;
+    margin: 10px auto;
+    
+ }
+ table thead {
+        background: #eee;
+        line-height: 50px;
+        
+ }
+ #blogDiv{
+    width: 70%;
+    margin: 20px auto;
+    text-align: left;
+ }
+ #blogDiv h3{
+    text-decoration: underline;
+    text-transform: capitalize;
+ }
+ #blogDiv p{
+    padding:10px 0  10px 30px;
+ }
+
+</style>
