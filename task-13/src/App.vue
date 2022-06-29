d71547cab02ffabc8b650ed0a363b4186fca2272
Ali Haider <alibarcha910@gmail.com>
2022-06-28 21:23:22 -0700

    form binding input,checkbox,radio and select

---
+++ task-13/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <user-app></user-app>
+  
+  </div>                                                                                                  
+</template>
+
+<script>
+import User from './components/User.vue'
+
+export default {
+  name: "App",
+  components: {
+    'user-app':User,
+    
+ 
+  },
+  data() {
+    return {
+    
+   
+    };
+  },
+  methods:{
+
+    }
+  }
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  text-align: center;
+}
+
+input,
+textarea {
+  margin: 10px;
+  width: 400px;
+  padding: 10px;
+}
+</style>
